4bab269e2bca35ed941862d300279cf1a3e9e70b
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-07-01 23:07:58 +0100

    Provide wrappers for 1, 8, 16, 32 and 64 bit integer types

---
+++ Sources/LLVM/LLVMInt32Type.swift
@@ -0,0 +1,20 @@
+import LLVMC
+
+@frozen
+public struct LLVMInt32Type: LLVMType {
+    
+    @usableFromInline
+    internal let _rawType: LLVMTypeRef
+    
+    @usableFromInline
+    internal let _context: LLVMContext
+    
+    @inlinable
+    public init(in context: LLVMContext) {
+        self._rawType = LLVMInt32TypeInContext(context._storage.rawContext)
+        self._context = context
+    }
+    
+    @inlinable
+    public var opaqueType: LLVMOpaqueType { LLVMOpaqueType(self) }
+}

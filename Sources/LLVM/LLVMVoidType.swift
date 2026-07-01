2025011130c9296c5ea0c825a868cb75504f5e89
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-07-01 20:34:46 +0100

    Declare LLVMVoidType, a strongly-typed wrapper

---
+++ Sources/LLVM/LLVMVoidType.swift
@@ -0,0 +1,16 @@
+import LLVMC
+
+public struct LLVMVoidType: LLVMType {
+    
+    @usableFromInline
+    internal let _rawType: LLVMTypeRef
+    
+    @usableFromInline
+    internal let _context: LLVMContext
+    
+    @inlinable
+    public init(in context: LLVMContext) {
+        self._rawType = LLVMVoidTypeInContext(context._storage.rawContext)
+        self._context = context
+    }
+}

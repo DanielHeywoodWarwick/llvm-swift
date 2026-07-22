bb8fed86d78f13e95c5a2a68c640750e8da53306
Daniel James Heywood <DanielHeywoodWarwick@icloud.com>
2026-07-21 20:40:40 -0700

    Declare a library product for LLVM

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "llvm swift",
+    products: [.library(name: "LLVM", targets: ["LLVM"])],
     targets: [
         .target(name: "LLVM", dependencies: ["LLVMC"]),
         .systemLibrary(name: "LLVMC", pkgConfig: "llvm")

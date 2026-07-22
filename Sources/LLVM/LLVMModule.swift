04b5360a0490d993e69a5d1ba46b46a730414db7
Daniel James Heywood <DanielHeywoodWarwick@icloud.com>
2026-07-21 20:47:31 -0700

    Declare LLVMModule, a wrapper for working with LLVMModuleRef

---
+++ Sources/LLVM/LLVMModule.swift
@@ -0,0 +1,57 @@
+import LLVMC
+
+@frozen
+public struct LLVMModule {
+    
+    @usableFromInline
+    internal final class _Storage {
+        
+        @usableFromInline
+        internal let rawModule: LLVMModuleRef
+        
+        @usableFromInline
+        internal let context: LLVMContext
+        
+        @inlinable
+        internal init(id: String, in context: LLVMContext) {
+            self.rawModule = LLVMModuleCreateWithNameInContext(
+                id,
+                context._rawContext
+            )
+            self.context = context
+        }
+        
+        @inlinable
+        deinit {
+            LLVMDisposeModule(rawModule)
+        }
+    }
+    
+    @usableFromInline
+    internal let _storage: _Storage
+    
+    @inlinable
+    public init(id: String, in context: LLVMContext) {
+        let storage = _Storage(id: id, in: context)
+        self._storage = storage
+    }
+    
+    @inlinable
+    internal var _rawModule: LLVMModuleRef { _storage.rawModule }
+}
+
+extension LLVMModule: Equatable {
+    
+    @inlinable
+    public static func == (lhs: LLVMModule, rhs: LLVMModule) -> Bool {
+        lhs._rawModule == rhs._rawModule
+    }
+}
+
+extension LLVMModule: Hashable {
+    
+    @inlinable
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(_rawModule)
+    }
+}

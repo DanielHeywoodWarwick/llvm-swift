1d0a419d035677845f7267d8fde5673b23b74984
Daniel James Heywood <DanielHeywoodWarwick@icloud.com>
2026-07-21 20:23:49 -0700

    Reinitialize Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,5 @@
 // swift-tools-version: 6.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let package = Package(
-    name: "llvm swift",
-    swiftLanguageModes: [.v6]
-)
+let package = Package(name: "llvm swift")
